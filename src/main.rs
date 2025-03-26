b925a603cea697c50b0e2fdc8713ed3f65ab871b
Remzi Cavdar <ict@remzi.info>
2025-03-26 17:07:47 +0100

    Small updates

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,17 @@ use rand::Rng;
 use console::*;
 
 enum ColorCode {
-    Error = 009,
-    Success = 010,
-    Info = 014,
+    Error = 9,
+    Success = 10,
+    Info = 14,
 }
 
 impl ColorCode {
     fn value(&self) -> u8 {
         match *self {
-            ColorCode::Error => 009,
-            ColorCode::Success => 010,
-            ColorCode::Info => 014,
+            ColorCode::Error => 9,
+            ColorCode::Success => 10,
+            ColorCode::Info => 14,
         }
     }
 }
@@ -29,7 +29,7 @@ fn main() {
              style(MIN_GUESS).color256(ColorCode::Info.value()),
              style(MAX_GUESS).color256(ColorCode::Info.value()));
 
-    let secret_number = rand::thread_rng().gen_range(MIN_GUESS..MAX_GUESS + 1);
+    let secret_number = rand::rng().random_range(MIN_GUESS..MAX_GUESS + 1);
 
     loop {
         print!("Please input your guess: ");
@@ -43,7 +43,7 @@ fn main() {
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => {
-                if num < MIN_GUESS || num > MAX_GUESS {
+                if !(MIN_GUESS..=MAX_GUESS).contains(&num) {
                     println!("{}", style(format!("Please enter a number between {} and {}!", MIN_GUESS, MAX_GUESS))
                         .color256(ColorCode::Error.value()));
                     continue;
